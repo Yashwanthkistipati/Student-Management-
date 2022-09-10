a14d6adaf939d64ec39d26b3ce2833b270f9583e
Yashwanthkistipati <reddy.k2123@gmail.com>
2022-09-10 16:11:46 +0530

    yashwanth upload

---
+++ touch.sh
@@ -0,0 +1,9 @@
+git init
+git remote add origin https://github.com/Yashwanthkistipati/Student-Management-.git
+git remote -v  
+git add . --force
+git commit -m "yashwanth upload"
+
+
+git push -f origin master
+
